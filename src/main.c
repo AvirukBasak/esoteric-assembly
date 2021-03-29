74ace8d5caefa93913cb59bc40bb54824d7df301
Oogle Glu <avirukbasak@gmail.com>
2021-03-29 21:11:50 +0530

    Update main.c

--- src/main.c
+++ src/main.c
@@ -1,8 +1,8 @@
-/* Version: 2021.3.25
+/* Version: 2021.3.30
  | Description: Another assembly simulator
  */
 
-#define VER "v2021.3.25"
+#define VER "v2021.3.30"
 
 #include "headers/headers.h"
 #include "global.c"
